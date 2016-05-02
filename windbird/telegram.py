b16896697a4f9985e03d2fb210eff8e90ce229c6
Nils Werner <nils.werner@audiolabs-erlangen.de>
2016-05-02 13:04:03 +0200

    disable web page preview

--- windbird/telegram.py
+++ windbird/telegram.py
@@ -4,4 +4,8 @@ import telegram
 
 def post(config, message):
     bot = telegram.Bot(token=config['api']['token'])
-    bot.sendMessage(chat_id=config['target']['chat_id'], text=message)
+    bot.sendMessage(
+        chat_id=config['target']['chat_id'],
+        text=message,
+        disable_web_page_preview=True,
+    )
